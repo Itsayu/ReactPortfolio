92e662a373512b66498f6ea08790828e6d187302
Ayush Kumar <94097145+Itsayu@users.noreply.github.com>
2023-05-30 10:15:52 +0530

    Update Loading.jsx

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -18,7 +18,7 @@ const Loading = ({isLoading}) => {
               <img
                 src={Logo4}
                 alt="Ayush Kumar Tiwari"
-                className="w-[80px] h-[80px]"
+                className="w-[90px] h-[80px]"
               />
           </motion.div>
         )
